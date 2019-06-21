7c827f0292d25e947c05ded30bfdfd731dee073c
glodsky <41773076+glodsky@users.noreply.github.com>
2019-06-21 16:04:48 +0800

    Update ffmpeg_concat.py

--- ffmpeg_concat.py
+++ ffmpeg_concat.py
@@ -25,9 +25,6 @@ def process_arguments():
 
 # process arguments
 args = process_arguments()
-#print(args.input)
-#print(args.output)
-#print(args.newfile)
 if args.input == None :
     args.input = os.curdir 
 sample_f_ext = '' # vidoe file extension 
